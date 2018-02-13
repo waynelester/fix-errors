c18d9b0ab2c2b6fd2ee33b50fd6a61f2b734db15
Peter Horton <phorton@softstackfactory.org>
2018-02-13 04:12:24 +0000

    changed let and if statements to reflect age instead of name, fixed log to include object reference

--- lesson8.js
+++ lesson8.js
@@ -4,13 +4,13 @@ var people = [
     { name: "John", age: 55 },
     { name: "Roger", age: 44 },
     { name: "Betty", age: 37 }
-    ]
+]
 
 var nameFilter = function() {
     for (var i = 0; i < people.length; i++) {
-        let name = people[i].name
-        if (name > 50) {
-            console.log("My Name is " + name)
+        let age = people[i].age
+        if (people[i].age < 50) {
+            console.log("My Name is " + people[i].name)
         }
     }
 }
