47142877f91508b5724ccf8c73e165e00136757c
Peter Horton <phorton@softstackfactory.org>
2018-02-13 04:52:42 +0000

    results.push argument changed to currentName from filteredName

--- lesson13.js
+++ lesson13.js
@@ -8,19 +8,19 @@ var people = [
     { name: "John", age: 55 },
     { name: "Roger", age: 44 },
     { name: "Betty", age: 37 }
-    ]
+]
 
-
+var results = [];
 var nameFilter = function(filteredName) {
-    var results = []
+
     for (var i = 0; i < people.length; i++) {
         let currentName = people[i].name
         if (currentName !== filteredName) {
-            results.push(filteredName)
+            results.push(currentName)
         }
     }
     return results
 }
 
 var filteredNames = nameFilter("Roger")
-console.log(filteredNames)
+console.log(results)
