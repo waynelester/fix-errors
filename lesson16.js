f60221203fa839e660dfd7e868d754bdb0d6771b
Peter Horton <phorton@softstackfactory.org>
2018-02-13 05:32:20 +0000

    voodoo magic! jk. made while loop for running

--- lesson16.js
+++ lesson16.js
@@ -12,17 +12,22 @@ var people = [
     { name: "John", age: 55 },
     { name: "Roger", age: 44 },
     { name: "Betty", age: 37 }
-    ]
+]
 
-
+let running = 0;
 var nameFilter = function(filteredName, anonymousFunction) {
     var results = []
     for (var i = 0; i < people.length; i++) {
+        while (running < i) {
+            running++;
+            console.log("This Ran " + ([i] - 1));
+        }
         let currentName = people[i].name
         if (currentName != filteredName) {
             results.push(currentName)
 
         }
+
     }
 
     return results
